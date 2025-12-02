68d25e71f7d4599babb85d5b9213e50b1d838a5b
MiletaBorisova <miletaborisova5@gmail.com>
2025-12-02 15:09:03 +0200

    add migration

---
+++ WebShopApp/WebShopApp.Infrastructure/Data/Entities/Order.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopApp.Infrastructure.Data.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+
+        [Required]
+        public string? UserId { get; set; }
+        public virtual ApplicationUser? User { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalPrice { get
+            {
+                return this.Quantity * this.Price - this.Quantity *
+            this.Price * this.Discount / 100;
+            } }
+    }
+}
